d7cbfcc6335405efe90196fc955c6777051a9add
Philippe Blanc <pblanc@zeeagency.com>
2019-07-03 07:59:07 +0200

    Lowercase and strip text + minor refactoring

--- fip_indicator.py
+++ fip_indicator.py
@@ -7,6 +7,8 @@ import urllib.request, json
 import time
 import threading
 import webbrowser
+import re
+import textwrap
 # import vlc
 
 gi.require_version('Gtk', '3.0')
@@ -15,21 +17,23 @@ from gi.repository import Gtk, AppIndicator3
 
 currpath = os.path.dirname(os.path.realpath(__file__))
 
+
 # Roadmap
-# If artist/title is mostly uppercase, switch to CamelCase
 # If artist name is too long, cut after X chars /spaces
 # If title name is too long, cut after X chars /spaces
+# Show edition year 
+# Link to album infos
 # Disc Cover
 # Play Station locally
 # Launch Station on Alexa device
 # Submit to last.fm
 
-class Indicator():
+class fipIndicator():
     def __init__(self):
-        self.app = 'update_setting'
-        iconpath = currpath+"/fip_100_color.png"
+        self.app = 'fipIndicator'
+        #iconpath = currpath+"/fip_100_color.png"
         self.indicator = AppIndicator3.Indicator.new(
-            self.app, iconpath,
+            self.app, os.path.abspath('fip_100_color.png'),
             AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         self.station = 7
@@ -38,11 +42,26 @@ class Indicator():
     def get_titles(self, data):
         #sep = Gtk.SeparatorMenuItem()
         for step in data['steps']:
-            # TODO : show edition year
-            menustring=data['steps'][step]['authors']+' - '+data['steps'][step]['title']
+
+            author=data['steps'][step]['authors']
+            if len(author) > 35:
+                author = textwrap.wrap(data['steps'][step]['authors'], 25)
+                author = author[0]+" ..."
+
+            title=data['steps'][step]['title']
+            if len(title) > 35:
+                title = textwrap.wrap(data['steps'][step]['title'], 25)
+                title = title[0]+" ..."
+
+            menustring=author+' - '+title
+
+            nb_uppercases = len(re.findall(r'[A-Z]',menustring))
+            if nb_uppercases > (len(menustring)/2):
+                menustring = menustring.title()
+
             if data['steps'][step]['start'] <= time.time()  <= data['steps'][step]['end']:
+                self.indicator.set_label(' '+menustring+' ', self.app)
                 menustring = '-- '+menustring+' --'
-                self.indicator.set_label(' '+data['steps'][step]['authors']+' - '+data['steps'][step]['title']+' ', self.app)
                 # TODO : Show Cover
             menuitem = Gtk.MenuItem(menustring)
             if 'path' in data['steps'][step] and data['steps'][step]['path']:
@@ -78,8 +97,9 @@ class Indicator():
         item_stations.set_submenu(sub_menu)
 
         item_quit = Gtk.MenuItem('Quit')
-        item_quit.connect('activate', self.stop)
+        item_quit.connect('activate', self.on_quit)
         self.menu.append(item_quit)
+
         self.menu.show_all()
         return self.menu
 
@@ -96,9 +116,9 @@ class Indicator():
         self.station = station["id"];
         self.refresh_menu()
 
-    def stop(self, source):
+    def on_quit(self, source):
         Gtk.main_quit()
-
-Indicator()
+        
+fipIndicator()
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 Gtk.main()
